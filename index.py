2f5ed4af6b738bbf7422c519e3e8551ecf70702a
Furqan Ali <47140274+Furqan001@users.noreply.github.com>
2025-05-11 17:25:57 +0500

    Update index.py

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ print('File Content:', content)
 os.remove('input.txt')
 
 with open('output.txt', 'a') as f:
-    f.write('Appended content\n')
+f.write('Appended content\n')
 
 with open('output.txt', 'r') as f:
     for line in f:
