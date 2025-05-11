c8651e5a9293faffe60cbe25f5a97616535ec1cb
Furqan Ali <47140274+Furqan001@users.noreply.github.com>
2025-05-11 17:10:11 +0500

    Create index.py

---
+++ index.py
@@ -0,0 +1,18 @@
+import os
+
+with open('output.txt', 'w') as f:
+f.write('Initial content\n')
+
+with open('input.txt', 'r') as f:
+    content = f.read()
+
+print('File Content:', content)
+
+os.remove('input.txt')
+
+with open('output.txt', 'a') as f:
+    f.write('Appended content\n')
+
+with open('output.txt', 'r') as f:
+    for line in f:
+        print(line.strip())
